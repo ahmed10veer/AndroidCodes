b1dd180910b4a2a74a82c6d5f4243a3693415e7b
ahmed10veer <ahmed10veer@gmail.com>
2017-09-26 14:47:45 +0600

    first commit

---
+++ app/src/main/java/com/ahmed10veer/androidcodes/FirstEmptyActivity.java
@@ -0,0 +1,13 @@
+package com.ahmed10veer.androidcodes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstEmptyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_empty);
+    }
+}
